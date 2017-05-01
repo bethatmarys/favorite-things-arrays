6121ce4427edc2fdb60c7e38e68e6fafa581d181
Beth Hansen and Ian Garner <beth+ian@>
2017-05-01 10:49:24 -0700

    add a working array and will show an output

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,12 @@
 $(document).ready(function() {
-  $("form#favoritethings").submit(function(event) {
-   var     event.preventDefault();
-    var car = $("input#car").val();
-    var vacation = $("input#vacation").val();
-    var food = $("input#food").val();
-    function getFields(input, field) {
-    var output = [];
-    for (var i=0; i < input.length ; ++i)
-        output.push(favoritethings[0]favoritethings[1]favoritethings[2]);
-        $("ul").prepend("<li>" + favorites2[0] + "</li>");
-        $("ul").prepend("<li>" + favorites2[1] + "</li>");
-        $("ul").prepend("<li>" + favorites2[2] + "</li>");
-    return output;
-
-var result = getFields(objArray, "foo");
-
-
-    }
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var car = $("select#car").val();
+    var vacation = $("select#vacation").val();
+    var food = $("select#food").val();
+    var favorites = [car, vacation, food];
+    $("ul").prepend("<li>" + favorites[0] + "</li>");
+    $("ul").prepend("<li>" + favorites[1] + "</li>");
+    $("ul").prepend("<li>" + favorites[2] + "</li>");
+  });
 });
