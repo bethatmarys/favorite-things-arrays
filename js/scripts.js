942c6cacf35d4091edf1032caaf449b0c07e2e3d
Beth Hansen and Ian Garner <beth+ian@>
2017-05-01 11:06:58 -0700

    add a push array

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+    var text =[];
     var car = $("select#car").val();
     var vacation = $("select#vacation").val();
     var food = $("select#food").val();
     var favorites = [car, vacation, food];
+    text.push(favorites[2], favorites[1], favorites[0])
     $("ul").prepend("<li>" + favorites[0] + "</li>");
     $("ul").prepend("<li>" + favorites[1] + "</li>");
     $("ul").prepend("<li>" + favorites[2] + "</li>");
+
   });
 });
